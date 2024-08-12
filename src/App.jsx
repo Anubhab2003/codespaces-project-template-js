dcb464050fcd20c7eadd3b5a53839c33141b8565
Anubhab Chowdhury <99398916+Anubhab2003@users.noreply.github.com>
2024-08-12 14:33:17 +0000

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Anubhab Chowdhury",
+  title: "Web Developer",
+  email: "anubhabchowdhury01@gmail.com",
+  gitHub: "Anubhab2003",
+  instagram: "Anubhab.2003",
+  linkedIn: "anubhab-chowdhury-63473b240",
+  // medium: "",
+  // twitter: "microsoft",
+  // youTube: "Code",
 };
 
 const primaryColor = "#4E567E";
